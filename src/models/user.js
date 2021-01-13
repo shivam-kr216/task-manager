ad6d1b6e021f063ee43b15c8bd351fc287adc2c8
shivam-kr216 <shivamkr216.com>
2021-01-13 19:47:13 +0530

    Commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -31,7 +32,13 @@ const userSchema = new mongoose.Schema({
     age: {
         type: Number,
         default: 0
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 //We can name the function anything i.e, findByCredentials
@@ -47,6 +54,17 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user;
 }
 
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    //it require two arguments first one will be the unique id 
+    //second is any stream of characters which is a signature
+    //third argument will be time expired
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisisnodetutorial'/*, {expiresIn: '7 days'}*/);
+    user.tokens = user.tokens.concat({ token: token });
+    await user.save();
+    return token;
+}
+
 //pre and post are two methods pre used to perform operation
 //before something and post after something
 //first argument is the event name after or before which something 
@@ -56,7 +74,7 @@ userSchema.pre('save', async function(next){
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password, 8)
     }
-    console.log('Completed');
+    //console.log('Completed');
     //next will tell that our operation will be finished otherwise it will be hanged
     next();
 })
--- src/router/user.js
+++ src/router/user.js
@@ -14,7 +14,8 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token})
     }
     catch (e) {
         res.status(400).send(e);
@@ -24,7 +25,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
     }catch(e) {
         res.status(400).send(e);
     }
@@ -77,7 +79,7 @@ router.patch('/users/:id', async (req, res) => {
     if (!isValidOperation) {
         return res.status(404).send({ error: 'Invalid updates!' });
     }
-
+ 
     try {
         //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
         const user = await User.findById(req.params.id)
